f323b4e15678ed51027af5fb81e3134752540cb7
sherpya <sherpya@netfarm.it>
2018-12-04 17:39:55 +0100

    clamwin win32 improvements

--- libclamav/others_common.c
+++ libclamav/others_common.c
@@ -584,6 +584,9 @@ int cli_ftw(char* path, int flags, int maxdepth, cli_ftw_cb callback, struct cli
     int ret;
 
     if(((flags & CLI_FTW_TRIM_SLASHES) || pathchk) && path[0] && path[1]) {
+#ifdef CLAMWIN
+        NORMALIZE_PATH(path, 0, return -1);
+#else
         char* pathend;
         /* trim slashes so that dir and dir/ behave the same when
 	 * they are symlinks, and we are not following symlinks */
@@ -593,6 +596,7 @@ int cli_ftw(char* path, int flags, int maxdepth, cli_ftw_cb callback, struct cli
         pathend = path + strlen(path);
         while(pathend > path && pathend[-1] == *PATHSEP) --pathend;
         *pathend = '\0';
+#endif /* CLAMWIN */
     }
     if(pathchk && pathchk(path, data) == 1)
         return CL_SUCCESS;
@@ -690,7 +694,9 @@ static int cli_ftw_dir(const char* dirname, int flags, int maxdepth, cli_ftw_cb 
                 sprintf(fname, PATHSEP "%s", dent->d_name);
             else
                 sprintf(fname, "%s" PATHSEP "%s", dirname, dent->d_name);
-
+#ifdef CLAMWIN
+            NORMALIZE_PATH(fname, 1, continue);
+#endif
             if(pathchk && pathchk(fname, data) == 1) {
                 free(fname);
                 continue;
@@ -1158,6 +1164,8 @@ cl_error_t cli_get_filepath_from_filedesc(int desc, char** filepath)
         goto done;
     }
 
+#elif CLAMWIN
+    return cw_get_filepath_from_filedesc(desc, filepath);
 #elif _WIN32
     DWORD dwRet = 0;
     intptr_t hFile = _get_osfhandle(desc);
