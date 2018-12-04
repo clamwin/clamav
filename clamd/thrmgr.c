e9873ae794ee3e024f0e9d48a6316c862885b03f
sherpya <sherpya@netfarm.it>
2018-12-04 17:39:55 +0100

    clamwin win32 improvements

--- clamd/thrmgr.c
+++ clamd/thrmgr.c
@@ -623,6 +623,10 @@ static void *thrmgr_worker(void *arg)
     int retval, must_exit = FALSE, stats_inited = FALSE;
     struct timespec timeout;
 
+#ifdef CLAMWIN
+    cw_disablefsredir();
+#endif
+
     /* loop looking for work */
     for (;;) {
         if (pthread_mutex_lock(&(threadpool->pool_mutex)) != 0) {
@@ -679,6 +683,9 @@ static void *thrmgr_worker(void *arg)
         logg("!Fatal: mutex unlock failed\n");
         exit(-2);
     }
+#ifdef CLAMWIN
+    cw_revertfsredir();
+#endif
     return NULL;
 }
 
