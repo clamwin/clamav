9e462c78d729dc09de70cdd0f6f95b9d7c4dfe8b
Gianluigi Tiesi <sherpya@gmail.com>
2025-03-17 06:12:22 +0100

    win32: avoid using cli_realpath on old windows

--- libclamav/others_common.c
+++ libclamav/others_common.c
@@ -1566,9 +1566,11 @@ cl_error_t cli_realpath(const char *file_name, char **real_filename)
         cli_warnmsg("cli_realpath: Invalid arguments.\n");
         goto done;
     }
-
 #ifdef _WIN32
-
+#if defined(_WIN32_WINNT) && _WIN32_WINNT <= _WIN32_WINNT_WINXP
+    *real_filename = strdup(file_name);
+    return CL_SUCCESS;
+#endif
     wpath = uncpath(file_name);
     if (!wpath) {
         errno = ENOMEM;
